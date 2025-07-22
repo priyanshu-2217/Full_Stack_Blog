d4815e12cd4053860c38fdb9c3bd6c3cfb06f4ed
Priyanshu Sharma <priyan90204@gmail.com>
2025-07-22 23:36:07 +0530

    Initialized

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Navbar from "./Components/Navbar";
+
+const App = () => {
+  return (
+    <div className="px-4 md:px-8 lg:px-16 lx:px-16 2lx:px-32">
+      <Navbar />
+    </div>
+  );
+};
+
+export default App;
